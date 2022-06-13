263c35b622ded90e075b7c6c547afc726fa82996
luchamarron <arnoldxchristopher@gmail.com>
2022-06-13 21:59:39 +0100

    Added Styling to form page and adapted server accordingly

--- go-server/main.go
+++ go-server/main.go
@@ -1,43 +1,58 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
+
 //function for the form page
-func formHandler(w http.ResponseWriter, r *http.Request){
-	if err := r.ParseForm(); err !=nil {
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
 	fmt.Fprintf(w, "POST request successful ")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
+	name := r.FormValue("walkname")
+	address := r.FormValue("date")
+	start := r.FormValue("start")
+	end := r.FormValue("end")
+	meetingpt := r.FormValue("meetingpt")
+	distance := r.FormValue("distance")
+	leader := r.FormValue("lead")
+	comments := r.FormValue("Comments")
 	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Date = %s\n", address)
+	fmt.Fprintf(w, "Start time = %s\n", start)
+	fmt.Fprintf(w, "End time = %s\n", end)
+	fmt.Fprintf(w, "Meeting point = %s\n", meetingpt)
+	fmt.Fprintf(w, "Leader = %s\n", leader)
+	fmt.Fprintf(w, "Distance = %s km\n", distance)
+	fmt.Fprintf(w, "Comments = %s km\n", comments)
 }
+
 //function for the /hello page
-func helloHandler(w http.ResponseWriter, r *http.Request){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-	if r.Method !="GET"{ //making sure that users are not able to submit POST
+	if r.Method != "GET" { //making sure that users are not able to submit POST
 		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
 	fmt.Fprintf(w, "Howdy doody!")
 }
+
 //main function of the webserver, depending on the url entered it will route the user to the form or the home page
-func main(){
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n") // The heart of the web server using the http import
-	if err :=http.ListenAndServe(":8080", nil); err !=nil { 
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
